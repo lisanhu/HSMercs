cfba048c88865a5e567e5a78763468b4c1080865
Sanhu Li <lisanhu2014@hotmail.com>
2022-12-20 04:42:19 -0500

    Updating single thread leaderboard tool to multi-thread. Update the jupyter notebook by adding text, not saving to disk by default, and other visualization improvements.

--- HSTools/LeaderBoardFetching/fetch.py
+++ HSTools/LeaderBoardFetching/fetch.py
@@ -1,9 +1,22 @@
 import requests as req
 import json
 import time
+import threading
 
-def single_download_task(url: str) -> list:
-    return req.get(url).json()["leaderboard"]["rows"]
+
+class DownloadClient(threading.Thread):
+    def __init__(self, url, pos, data_store):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.pos = pos
+        self.data = data_store
+    
+    def run(self):
+        # print("Downloading page {}...".format(self.pos + 1))
+        res = req.get(self.url).json()["leaderboard"]["rows"]
+        assert(len(self.data) > self.pos)
+        self.data[self.pos] = res
+
 
 def download_leaderboard_data(path: str) -> list:
     root_url = "https://hearthstone.blizzard.com/en-us/api/community/leaderboardsData?region=US&leaderboardId=mercenaries"
@@ -12,25 +25,34 @@ def download_leaderboard_data(path: str) -> list:
 
     current_season = resp["seasonMetaData"]["US"]["mercenaries"]["seasons"][0]
     total_pages = resp["leaderboard"]["pagination"]["totalPages"]
-    rows = []
 
     print("Current seasonId is {}, total pages: {}".format(current_season, total_pages))
     start_time = time.time()
+    data_store = [[]] * total_pages
+    threads = []
     for pg in range(total_pages):
         url = root_url + "&seasonId={}&page={}"
         url = url.format(current_season, pg + 1)
-        print("Downloading page {}...".format(pg + 1))
-        resp = req.get(url).json()
-        rows = rows + resp["leaderboard"]["rows"]
+        t = DownloadClient(url, pg, data_store)
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
     
     end_time = time.time()
-    print("Done downloading. Time taken {} seconds.", end_time - start_time)
+    print("Done downloading. Time taken {} seconds.".format(end_time - start_time))
 
-    with open(path, "w") as db:
-        db.write(json.dumps(rows))
-    print("Done writing to file {}".format(path))
+    rows = []
+    for page_data in data_store:
+        rows += page_data
+
+    if path != None:
+        with open(path, "w") as db:
+            db.write(json.dumps(rows))
+        print("Done writing to file {}".format(path))
 
     return rows
 
 if __name__ == "__main__":
-    download_leaderboard_data("leaderboard.data")
+    print(download_leaderboard_data(None))
